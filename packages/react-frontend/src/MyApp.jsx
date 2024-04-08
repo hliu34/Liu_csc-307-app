e4328b992e82f2c4e2d08f7e409a2045c7b51e0d
Haowy012 <hliu.xian025@gmail.com>
2024-04-08 09:54:46 -0700

    implemented formto take in inputs to be added into table

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,27 +1,12 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
+
+
 
 function MyApp() {
-    const [characters, setCharacters] = useState([
-        {
-            name: "Charlie",
-            job: "Janitor"
-        },
-        {
-            name: "Mac",
-            job: "Bouncer"
-        },
-        {
-            name: "Dee",
-            job: "Aspring actress"
-        },
-        {
-            name: "Dennis",
-            job: "Bartender"
-        }
-
-    ]);
+    const [characters, setCharacters] = useState([]);
 
     return (
         <div className="container">
@@ -29,6 +14,7 @@ function MyApp() {
                 characterData={characters}
                 removeCharacter={removeOneCharacter}
             />
+            <Form />
         </div>
     );
 
