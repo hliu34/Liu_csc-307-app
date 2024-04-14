698b859a6a3d1a5921fc1fb99247970d04f5cea0
hliu34 <hliu.xian025@gmail.com>
2024-04-14 14:37:56 -0700

    updated

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -33,6 +33,24 @@ const users = {
         }
     ]
 };
+
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+        (user) => user["name"] === name
+    );
+};
+
+app.get("/users", (req, res) => {
+    const name = req.query.name;
+    if (name != undefined) {
+        let result = findUserByName(name);
+        result = { users_list: result };
+        res.send(result);
+    } else {
+        res.send(users);
+    }
+});
+
 app.get("/users", (req, res) => {
     res.send(users);
 });
@@ -40,7 +58,7 @@ app.get("/users", (req, res) => {
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello Danny!");
 });
 
 app.listen(port, () => {
@@ -48,3 +66,6 @@ app.listen(port, () => {
         `Example app listening at http://localhost:${port}`
     );
 });
+
+
+
