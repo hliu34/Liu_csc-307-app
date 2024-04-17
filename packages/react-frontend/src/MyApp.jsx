4514309d89baa07131b7f3bae136b0ec0cd884f6
hliu34 <hliu.xian025@gmail.com>
2024-04-17 09:43:11 -0700

    initializing link between frontend and backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,15 +1,28 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
 import Table from "./Table";
 import Form from "./Form";
+
+
 
 
 
 function MyApp() {
     const [characters, setCharacters] = useState([]);
 
+    useEffect(() => {
+        fetchUsers()
+            .then((res) => res.json())
+            .then((json) => setCharacters(json["users_list"]))
+            .catch((error) => { console.log(error); });
+    }, []);
+
     function updateList(person) {
-        setCharacters([...characters, person]);
+        postUser(person)
+            .then(() => setCharacters([...characters, person]))
+            .catch((error) => {
+                console.log(error);
+            })
     }
 
     function removeOneCharacter(index) {
@@ -29,8 +42,22 @@ function MyApp() {
         </div>
     );
 
+    function fetchUsers() {
+        const promise = fetch("http://localhost:8000/users");
+        return promise;
+    }
 
+    function postUser(person) {
+        const promise = fetch("Http://localhost:8000/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(person),
+        });
 
+        return promise;
+    }
 
 }
 
